5138092cb58603c267cd6450a97f8284086c8c60
Henrique Bispo <henriquefbispo.dev@gmail.com>
2025-07-09 23:02:00 -0300

    implementa login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,28 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
+import { AuthLayout } from './pages/auth-layout/auth-layout';
+import { Login } from './components/login/login';
+import { Register } from './components/register/register';
 
 export const routes: Routes = [
     {
         path: '',
         component: Home,
+    },
+    {
+        path: '',
+        component: AuthLayout,
+        children: [
+            {
+                path:'login',
+                component: Login,
+                pathMatch: 'full'
+            },
+            {
+                path: 'register',
+                component: Register,
+                pathMatch: 'full'
+            }
+        ]
     }
 ];
--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
-  selector: 'app-home',
+  selector: 'app-login',
   imports: [RouterLink],
-  templateUrl: './home.html',
-  styleUrl: './home.scss'
+  templateUrl: './login.html',
+  styleUrl: './login.scss'
 })
-export class Home {
+export class Login {
 
 }
