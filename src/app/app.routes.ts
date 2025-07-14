58e831843ecb8bdbf886bb475b858b8e3d4cfae7
Henrique Bispo <henriquefbispo.dev@gmail.com>
2025-07-14 19:12:47 -0300

    implementa register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
-import { AuthLayout } from './pages/auth-layout/auth-layout';
+import { AuthLayout } from './pages/layouts/auth-layout/auth-layout';
 import { Login } from './components/login/login';
 import { Register } from './components/register/register';
+import { MainLayout } from './pages/layouts/main-layout/main-layout';
+import { Dashboard } from './components/dashboard/dashboard';
 
 export const routes: Routes = [
     {
@@ -21,6 +23,17 @@ export const routes: Routes = [
             {
                 path: 'register',
                 component: Register,
+                pathMatch: 'full'
+            }
+        ]
+    },
+    {
+        path: '',
+        component: MainLayout,
+        children: [
+            {
+                path:'dashboard',
+                component: Dashboard,
                 pathMatch: 'full'
             }
         ]
---
+++ src/app/pages/layouts/main-layout/main-layout.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-main-layout',
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './main-layout.html',
+  styleUrl: './main-layout.scss'
+})
+export class MainLayout {
+
+}
